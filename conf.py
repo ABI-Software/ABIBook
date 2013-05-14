cf912fce3af2c981ea067e2dd80926a0d8450891
David Nickerson <nickerso@users.sourceforge.net>
2013-05-15 11:55:30 +1200

    trying to find a last_updated format that works with readthedocs

--- conf.py
+++ conf.py
@@ -140,7 +140,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-html_last_updated_fmt = '%+'
+html_last_updated_fmt = '%F %R %Z'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
